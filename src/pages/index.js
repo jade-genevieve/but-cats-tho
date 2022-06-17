ae322e39fa9bfc61a7f37831d14cbebf74bdeb39
Jade Slade <26174144+jade-genevieve@users.noreply.github.com>
2022-06-17 14:24:13 +0100

    Fix spaces. Need to add eslint...

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,15 @@ import React from "react"
 
 const Home = () => {
   return (
-   
-  <html lang="en">
-    <head>
-      <title>Just random stuff about cats</title>
-    </head>
-  <body>
-   <h1>But Cats Tho</h1>
-  </body>
-</html>
-    
+    <html lang="en">
+        <head>
+          <title>Just random stuff about cats</title>
+        </head>
+        <body>
+          <h1>But Cats Tho</h1>
+        </body>
+    </html>    
   )
 }
-
 
 export default Home
